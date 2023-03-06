a386468e815c3cde794b3e034af4d90a1c01cc98
Daniel Visicatto <dpfreestyle@gmail.com>
2023-03-06 20:03:03 -0300

    Add project files.

---
+++ ConverteGrausCelsiusParaGrausFareheit/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Digite a temperatura em ºC: ");
+
+double temperaturaCelsius = double.Parse(Console.ReadLine());
+double temperaturaFareheit = (double)(temperaturaCelsius * 1.8) + 32;
+
+Console.WriteLine("O equivalente em Farenheit é: {0}ºF", temperaturaFareheit);
